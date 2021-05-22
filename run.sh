22dad79a7290d3fbce1e7bc655ced9e7cff3db4a
derrick921213 <dlin12457@gmail.com>
2021-05-22 17:03:02 +0800

    test

--- run.sh
+++ run.sh
@@ -4,12 +4,18 @@ mv server.tar.gz flask-server
 basedir=$(dirname "$0")
 cgi=$basedir/cgi-server
 flask=$basedir/flask-server
+mc=$basedir/minecraft-server
+
 # Build custom image.
 docker build -t flask-server "$flask"
-docker build -t cgi-server "$basedir2"
+docker build -t cgi-server "$cgi"
+docker build -t mc-server "$mc"
+
 # Stop the container created before
-docker stop flask-test cgi-test
+docker stop flask-test cgi-test mc-test
 cd $basedir/docker/website/cgi
 #Run docker container.
 docker run -d --rm -p 8081:80 --name cgi-test -v $PWD:/usr/local/apache2/cgi-bin/ cgi-server
 docker run -d --rm -p 5000:5000 --name flask-test flask-server
+cd ../../../minecraft-server
+docker run -d --rm -p 25565:25565 -p 25575:25575 --name mc-test -v $PWD:/mcserver mc-server
